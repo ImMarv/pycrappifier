2a96c1b2f927dcae5ad7fbff4a919ce6290fe520
Marv <94709651+ImMarv@users.noreply.github.com>
2026-05-22 16:55:15 +0100

    removed 2 controller system to a single unified one

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ except ImportError:
         except ImportError:
             from PySide6.QtWidgets import QApplication
 from ui.AudioExporterUi import AudioExporterUI
-from app.MainWindow import MainWindow
 
 def main():
     app = QApplication(sys.argv)
     window = AudioExporterUI()
-    logic = MainWindow(window)
     window.show()
     # support bindings that expose exec() instead of exec_()
     exec_func = getattr(app, "exec_", getattr(app, "exec"))
